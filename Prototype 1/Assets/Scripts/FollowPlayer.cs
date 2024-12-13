0265c9126e3cdd2480064f7360aa61ab1fb53d16
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2024-12-13 15:22:50 -0400

    Lesson 1.4 Completed!

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,9 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 offset = new Vector3((float)0.0, (float)4.5, (float)-8.39);
+    //Note: tutorial said to have this private not public
+    public Vector3 offset = new Vector3((float)0.0, (float)4, (float)-7.0);
+    public Vector3 coffset = new Vector3((float)0.0, (float)4, (float)-7.0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +18,9 @@ public class FollowPlayer : MonoBehaviour
     //Late Update() is different because it will happen after the update method happens this remove the jitter with the vehicle/Camera
     void LateUpdate()
     {
+        
         //Offsets the camera behind the player by adding to the player's position 
         transform.position = player.transform.position + offset;
+
     }
 }
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //Stat is called before the first frame update - JM
@@ -15,8 +19,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        //Moves the Vehicle Forward
+        
+        //This where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //We move the Vehicle Forward here
         // transform.Translate(0,0,1); // this moveds the object forward the object forwards 1 meter 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); //Time.deltaTime Forces the game to run no matter what how many frames are made
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); //Time.deltaTime Forces the game to run no matter what how many frames are made
+                                                                                      //Currently moving forward 5 m pre second. 
+
+        //Move the vehicle left or right. 
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //Makes the car rotate while turn unlike the line above when turning.
+        //This also turns the vehicle
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+      
+
     }
 }
